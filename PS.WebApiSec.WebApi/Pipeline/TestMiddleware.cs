87a46e1f99cf41980f465df20511d93bdfcaa170
Michael Demarco <miczdem@gmail.com>
2015-12-15 17:40:35 +1100

    testing the middleware pipeline by creating a user

--- PS.WebApiSec.WebApi/Pipeline/TestMiddleware.cs
+++ PS.WebApiSec.WebApi/Pipeline/TestMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Owin;
@@ -20,6 +21,11 @@ namespace PS.WebApiSec.WebApi.Pipeline
         {
             //"server.user" is the key for the principal in the dictionary, or use the OwinContext wrapper
             var context = new OwinContext(env);
+
+            // authentication
+            context.Request.User = new GenericPrincipal(
+                new GenericIdentity("dummy"),new string [] {} );
+
             Helper.Write("Middleware", context.Request.User);
             await _next(env);
         }
